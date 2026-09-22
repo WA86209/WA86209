9ff5f6c051a5a7cfbf3b2798845a8f5b975b5c06
WA86209 <2022wa86209@wilp.bits-pilani.ac.in>
2026-09-23 01:37:37 +0530

    Update App.java

--- src/main/java/com/wa86209/App.java
+++ src/main/java/com/wa86209/App.java
@@ -2,6 +2,6 @@ package com.wa86209;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from WA86209 Jenkins CI");
+        System.out.println("Hello from WA86209 Jenkins CI - Task 4");
     }
 }
